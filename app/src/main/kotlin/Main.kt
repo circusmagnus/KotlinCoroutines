611cc8b763f7521610a745380cd59b3abfcef492
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2019-07-13 16:13:36 +0200

    Clean unneeded classes

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 import data.OffersRepository
-import data.TestClass
 
 fun main(){
     repeat(3){ Thread.sleep(200) ; print(".") }
     OffersRepository().getOffers("Krzesło")
     print("done")
     repeat(3){ Thread.sleep(200) ; print(".") }
-    TestClass()
-
 }
