dc0343bdc2dc95fab160b2e16f39ce25ca7e0107
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2019-07-15 23:11:45 +0200

    cwiczenie-6 prepare naive implementation

--- app/src/main/kotlin/Playground.kt
+++ app/src/main/kotlin/Playground.kt
@@ -12,7 +12,17 @@ class Playground(
 
 
     fun showSortedOffers(queries: List<String>) {
+        runBlocking {
+            queries
+                .map { query ->
+                    launch {
+                        val offers = getOffers(query)
+                        val sorted = offers.sorted()
+                        sorted.forEach { display.showNewLine(it.toString()) }
 
+                    }
+                }
+        }
     }
 
     private suspend fun getOffers(query: String): List<Offer> = withContext(Dispatchers.Default) {
