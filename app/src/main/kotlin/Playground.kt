3651d6220d4ad2a4de7ba22504c1acae3f0e1ccd
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2019-10-23 23:15:35 +0200

    Remove unneeded methods

--- app/src/main/kotlin/Playground.kt
+++ app/src/main/kotlin/Playground.kt
@@ -16,49 +16,14 @@ class Playground(
         launch { runDotAnim() }
     }
 
-    fun showOffersWithQuery(query: String) {
-        launch {
-            val offers = getOffers(query)
-            display.showNewLine("Done. Offers: $offers")
-        }
-    }
-
     private suspend fun runDotAnim() = coroutineScope {
         while (isActive) {
             delay(200); display.showNewLine(".")
         }
     }
 
-    fun showSellersWithOffer(offerQuery: String) {
-        launch {
-            val sellers = getSellersForOffer(offerQuery)
-            display.showNewLine("Done. Sellers: $sellers")
-        }
-    }
-
     private suspend fun getOffers(query: String): List<Offer> = withContext(Dispatchers.IO) {
         offersRepository.getOffersBlocking(query)
-    }
-
-//        suspendCoroutine { coroutine ->
-//        offersRepository.getOffersAsync(query) { offers ->
-//            coroutine.resume(offers)
-//        }
-//    }
-
-    private suspend fun getSellers() = withContext(Dispatchers.IO) {
-        sellersRepository.getSellersBlocking()
-    }
-
-    private suspend fun getSellersForOffer(offerQuery: String): List<Seller> = coroutineScope {
-        val getOffers = async { getOffers(offerQuery) }
-        val getSellers = async { getSellers() }
-
-        getSellers.await().filterSellingOffers(getOffers.await())
-    }
-
-    private fun List<Seller>.filterSellingOffers(offers: List<Offer>) = filter { seller ->
-        offers.any { offer -> seller.offerIds.contains(offer.id) }
     }
 
     fun showSortedOffers(queries: List<String>) {
