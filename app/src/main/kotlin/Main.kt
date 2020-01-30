0f7faf0ef90ec27ffbdae7460770283e051024ee
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2020-01-30 08:54:31 +0100

    Ask for 10_000_000 queries for sellers with offer

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,12 +1,31 @@
 import kotlinx.coroutines.*
+import kotlin.random.Random
 
 fun main() {
     runBlocking {
         val playgroundScope = this + Job(parent = coroutineContext[Job])
         val playground = Playground(OffersRepository(), SellersRepository(), ConsoleDisplay(), playgroundScope)
 
+        val availableNames = listOf(
+            "Krzesło",
+            "Łóżko",
+            "Telefon",
+            "Rower",
+            "Spodnie",
+            "Zegarek",
+            "Fortepian",
+            "Mandarynka",
+            "Wół",
+            "Fajka"
+        )
+
+        val queries = generateSequence { Random.nextInt(0, availableNames.lastIndex) }
+            .take(10_000_000)
+            .map { availableNames[it] }
+            .toList()
+
         playground.startAnimation()
-        playground.showSellersForOffers(listOf("Krzesło", "Telefon", "Rower"))
+        playground.showSellersForOffers(queries)
         delay(10_000)
         playground.cancel()
         println("Canceled!")
